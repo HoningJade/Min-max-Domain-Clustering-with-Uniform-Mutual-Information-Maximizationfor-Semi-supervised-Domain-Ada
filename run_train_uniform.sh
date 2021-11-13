ea4dec235fbca577549b3bc147349ce42b1a9428
duyxxd <duyxxd@umich.edu>
2021-11-13 23:35:26 +0000

    early stop

--- run_train_uniform.sh
+++ run_train_uniform.sh
@@ -2,4 +2,4 @@
 CUDA_VISIBLE_DEVICES=$1 python main.py \
   --method $2 --dataset multi \
   --source real --target sketch \
-  --net $3 --save_check --checkpath $4 --bs 24 --uniform_sampling --pseudo_balance_target --early
+  --net $3 --save_check --checkpath $4 --bs 24 --uniform_sampling --pseudo_balance_target
