a59162989da7fc56ce05aa140fc8db7b55888f85
duyxxd <duyxxd@umich.edu>
2021-11-13 01:36:32 +0000

    get prediction

--- utils/return_dataset.py
+++ utils/return_dataset.py
@@ -2,7 +2,7 @@ import os
 import torch
 from torchvision import transforms
 from loaders.data_list import Imagelists_VISDA, return_classlist
-from torch.utils.data.sampler import WeightedRandomSampler
+from torch.utils.data.sampler import SubsetRandomSampler, WeightedRandomSampler
 from collections import Counter
 import numpy as np
 
@@ -25,19 +25,24 @@ def return_uniform_sampler(label_list, num_class=125):
         data sampler based on given datalist
     """
     count_dict = Counter(label_list)
-    count_dict_full = {k:0 for k in range(num_class)}
+    count_dict_full = {k: 0 for k in range(num_class)}
     for k, v in count_dict:
         count_dict_full[k] = v
-    count_dict_sorted = {k: v for k, v in sorted(count_dict_full.items(), key=lambda item: item[0])}
+    count_dict_sorted = {k: v for k, v in sorted(
+        count_dict_full.items(), key=lambda item: item[0])}
     class_sample_count = np.array(list(count_dict_sorted.values()))
     class_sample_count = class_sample_count / class_sample_count.max()
     class_sample_count += 1e-8
-
     weights = 1 / torch.Tensor(class_sample_count)
+
     sample_weights = [weights[l] for l in label_list]
     sample_weights = torch.DoubleTensor(np.array(sample_weights))
     sampler = WeightedRandomSampler(sample_weights, len(sample_weights))
     return sampler
+
+
+def return_pseudo_label(G, F1, ):
+    pass
 
 
 def return_dataset(args):
@@ -102,14 +107,24 @@ def return_dataset(args):
         bs = 32
     else:
         bs = 24
+
+    if args.uniform_sampling:
+        source_sampler = return_uniform_sampler(source_dataset.labels)
+        target_sampler = return_uniform_sampler(target_dataset.labels)
+    else:
+        source_sampler = SubsetRandomSampler(np.arange(len(source_dataset.imgs)))
+        target_sampler = SubsetRandomSampler(np.arange(len(target_dataset.imgs)))
+
     source_loader = torch.utils.data.DataLoader(source_dataset, batch_size=bs,
                                                 num_workers=3, shuffle=True,
-                                                drop_last=True)
+                                                drop_last=True,
+                                                sampler=source_sampler)
     target_loader = \
         torch.utils.data.DataLoader(target_dataset,
                                     batch_size=min(bs, len(target_dataset)),
-                                    num_workers=3,
-                                    shuffle=True, drop_last=True)
+                                    num_workers=3, shuffle=True, 
+                                    drop_last=True,
+                                    sampler=target_sampler)
     target_loader_val = \
         torch.utils.data.DataLoader(target_dataset_val,
                                     batch_size=min(bs,
