eed45a0497670624ff61d69e9025524ffd93e3dd
duyxxd <duyxxd@umich.edu>
2021-12-08 08:52:19 +0000

    add weight arg

--- run_train_JSDsrcT.sh
+++ run_train_JSDsrcT.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=$1 python main.py \
   --method MME --dataset multi \
   --source real --target sketch \
   --net resnet34 --save_check --checkpath /datab/duyxxd/DNet/uni_jsd_truesrc --bs 24 \
-  --momentum 0.99 --use_true_src
+  --momentum 0.99 --use_true_src --smlbd $2
